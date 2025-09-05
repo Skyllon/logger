4dab96a437e7bf2e5b17403a2c8411aba25e2dc3
Skyllon <skylon.sys@gmail.com>
2025-09-05 18:16:05 +0700

    added prototype of function that will write log

--- include/processing.h
+++ include/processing.h
@@ -13,4 +13,15 @@ typedef enum log_type {
     Failed,
 } log_type_t;
 
+/*
+    @brief: write log with any message to file
+
+    @param: filepath - path to file
+    @param: message - message to write in log file
+    @param: level - level of awareness
+
+    @return: write in file in exact format
+*/
+void write_log(const char *filepath, const char *message, const log_type_t level);
+
 #endif // PROCESSING_H
